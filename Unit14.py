8f21692023ce85592c59e38fd6af0c013f91bf2f
rn25-long-app <bhlong3112@gmail.com>
2024-07-05 13:23:19 +0700

    add LENGTH & COUNT

--- Unit14.py
+++ Unit14.py
@@ -15,15 +15,29 @@ print('Check result :', colors)
 
 # Example 2: 
 # List [] will start : 0 to .... , to left to Right :
-studentList = ["Peter", "Rush", "Micheal", "Obama", "Trump", "Biden" , "Clinton", "Snoop-dogg",'Joinwick', 'Joincena','Rechar','Monica']
+studentList = ["Peter", "Rush", "Rush" , "Micheal", "Obama", "Trump", "Biden" , "Clinton", "Snoop-dogg",'Joinwick', 'Joincena','Rechar','Monica']
 print('Filter name in Lists :', studentList[2])
 print('Filter name in Lists :', studentList[6])
 print('Filter name in Lists :', studentList[:])
 print('Filter name in Lists :', studentList[0 : 4])
 print('Filter name in Lists :', studentList[4 : 20])
+
 # .append()/add data in studentList : 
 studentList.append('Chang')
 print('New list students :', studentList)
-
 studentList.append('Justin')
 print('New list students :', studentList)
+
+# len(): /add data to the end of the list :
+studentList[len(studentList):]= ['Ti anh'] 
+print('New list students :', studentList)
+
+# .insert()/ insert data in studentList:
+studentList.insert(2, 'Teo em')
+print('New list students :', studentList)
+
+# Number of elements in the list :
+print('Length of the list :',len(studentList))
+
+# count() /Count the number of elements that satisfy the condition :
+print('Count :', studentList.count('Rush'))
