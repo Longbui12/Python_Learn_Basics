e8b854359d06c372d83a52f94fef5b2673df6183
Peter_Chang <bhlong3112@gmail.com>
2024-07-06 11:57:29 +0700

    completed exercise of loop FOR & nested loop FOR , remember again exercise

---
+++ Loop_FOR/Practice_Loop_For.py
@@ -0,0 +1,15 @@
+# Practice about loop FOR :
+# Exercise: Print Multiplication table (Bảng cửu chương):
+
+# ex1 : Print Multiplication table of 2 x :
+for i in range(1,11):
+    print('2 x {0} = {1}'.format(i, 2 * i))
+
+
+# ex2 : Print all Multiplication table : dùng vòng lặp vòng nhau :
+for j in range(2 , 10 ):
+    print('Multiplication table :', j)
+    for i in range(1, 11):
+        print('{0} x {1} = {2}'.format(j, i, j * i))
+
+# Home work : again practice exercise above (loop 'FOR' & nested loop 'FOR')
