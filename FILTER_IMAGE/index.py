bbd6841be0e63d295b0c47e3141bc5109b8661e7
rn25-long-app <bhlong3112@gmail.com>
2024-07-08 13:55:09 +0700

    Update code for file FILTER_IMAGE

--- FILTER_IMAGE/index.py
+++ FILTER_IMAGE/index.py
@@ -102,47 +102,47 @@ def hash_file(file_path):
         hasher.update(buf)
     return hasher.hexdigest()
 
-# Đường dẫn tới thư mục chứa các ảnh cần sao chép
-source_dir = 'D:\Image 1'  # Thay thế bằng đường dẫn thực tế (đường dẫn file cần sao chép)
-destination_dir = 'D:\Image 2'  # Thay thế bằng đường dẫn thực tế (đường dẫn file được sao chép)
+# Đường dẫn tới thư mục chứa các ảnh cần sao chép : (đường dẫn sẽ dùng dấu / or \\ )
+source_dir = 'C:/Users/lenovo/OneDrive/Desktop/picture project hospital/Mẫu logo làm áo/Ảnh chế'  # Thay thế bằng đường dẫn thực tế (đường dẫn file cần sao chép)
+destination_dir = 'C:\\Users\\lenovo\\OneDrive\\Desktop\\COPY_IMAGES'  # Thay thế bằng đường dẫn thực tế (đường dẫn file được sao chép)
 
 try:
-    # Kiểm tra xem thư mục đích có tồn tại hay không, nếu không thì tạo mới
+    # Kiểm tra xem thư mục đích có tồn tại hay không, nếu không thì tạo mới :
     if not os.path.exists(destination_dir):
         os.makedirs(destination_dir)
 
-    # Tạo một bộ để theo dõi các hàm băm của các tệp đã được sao chép
+    # Tạo một bộ để theo dõi các hàm băm của các tệp đã được sao chép :
     copied_hashes = set()
 
-    # Biến đếm số lượng ảnh đã sao chép
+    # Biến đếm số lượng ảnh đã sao chép :                         
     copied_count = 0
 
-    # Lặp qua tất cả các tập tin trong thư mục nguồn
+    # Lặp qua tất cả các tập tin trong thư mục nguồn :
     for filename in os.listdir(source_dir):
-        # Kiểm tra xem tập tin có phải là ảnh hay không (ví dụ như .jpg, .png, .jpeg)
-        # Có thể thêm các định dạng ảnh khác vào hàm ở đưới đây .
+        # Kiểm tra xem tập tin có phải là ảnh hay không (ví dụ như .jpg, .png, .jpeg) :
+        # Có thể thêm các định dạng ảnh khác vào hàm ở đưới đây :
         if filename.endswith(('.jpg', '.png', '.jpeg', '.svg', '.webp', '.JPG')):
             # Tạo đường dẫn đầy đủ tới tập tin nguồn
             source_path = os.path.join(source_dir, filename)
 
-            # Tính toán hàm băm của tập tin
+            # Tính toán hàm băm của tập tin :
             file_hash = hash_file(source_path)
 
             if file_hash not in copied_hashes:
-                # Thêm hàm băm vào bộ
+                # Thêm hàm băm vào bộ :
                 copied_hashes.add(file_hash)
                 
-                # Tạo đường dẫn đầy đủ tới tập tin đích
+                # Tạo đường dẫn đầy đủ tới tập tin đích :
                 destination_path = os.path.join(destination_dir, filename)
                 
-                # Sao chép tập tin từ thư mục nguồn sang thư mục đích
+                # Sao chép tập tin từ thư mục nguồn sang thư mục đích :
                 shutil.copy(source_path, destination_path)
                 print(f"Sao chép từ file gốc : {filename} ==> Đi tới File ==> {destination_dir}")
                 
-                # Tăng biến đếm lên sau khi sao chép thành công
+                # Tăng biến đếm lên sau khi sao chép thành công :
                 copied_count += 1
 
-    print(f"Đã sao chép được : {copied_count} ảnh từ thư mục nguồn tới thư mục đích, không có tệp trùng lặp.")
+    print(f"Đã sao chép được : {copied_count} ảnh từ thư mục gốc tới thư mục đích, không có tệp trùng lặp.")
 
 except Exception as e:
     print(f"Đã xảy ra lỗi: {e}")
