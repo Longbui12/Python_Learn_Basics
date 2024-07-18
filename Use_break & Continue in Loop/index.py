896dbe9cca6be02177c11fcce3f2c5819b83b214
Peter_Chang <bhlong3112@gmail.com>
2024-07-18 17:02:02 +0700

    Learn BREAK & CONTINUE OF PYTHON

---
+++ Use_break & Continue in Loop/index.py
@@ -0,0 +1,22 @@
+# ex1 : bài tập cơ bản về vòng lặp for :
+for i in range (0, 11) :
+    print(i)
+
+print('==================================')
+
+# ex2: Câu lệnh có BREAK với vòng lặp FOR :
+for i in range (0,10):
+    print(i)
+    if(i > 5):
+        break
+print('==================================')
+# ex3: Cậu lệnh có BREAK với vòng lặp WHILE :
+n = 100
+while (n > 0):
+    print(n)
+    n = n // 2
+    if(n < 50):
+        break
+
+# ex4: Vòng lặp lồng nhau :
+
