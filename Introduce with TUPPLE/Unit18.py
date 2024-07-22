6572293373676e74c17bcff7bc3141a70463e53e
rn25-long-app <bhlong3112@gmail.com>
2024-07-23 01:09:44 +0700

    Learn about Tupple....., to be contenie ....

---
+++ Introduce with TUPPLE/Unit18.py
@@ -0,0 +1,34 @@
+# Introduce about TUPPLE :
+gender = ('man', 'woman')
+print(gender[0])
+
+classes = (1,2,3,4,5,6,7,8,9,10)
+print(classes[0 : 3])
+
+fruit = ('Apple', 'Banana','Apple','Orange', 'Lemon', 'Melon', 'Coconut')
+print(fruit[0: 3])
+
+# classes[0] = 13
+#ex 2 : loop with Tupple :
+for x in fruit:
+    print(x)
+
+# Ex3: plus 2 Tupples:
+y = (1,2,3) + (4,5,6)
+print(y)
+
+# Ex4: multiply 2 Tupples:
+z = (1,2,3) * 2
+print(z)
+
+# Ex5: dò xem phần tử có trong Tupple không IN
+print("boom" in fruit)
+
+#ex 6: tính độ dài của Tupple use LEN :
+x = len(fruit)
+print(x)
+
+#ex7: đếm số lượng của các phần tử trong TUPPLE :
+print(fruit.count('Apple'))
+
+print(fruit.count('Xoài'))
