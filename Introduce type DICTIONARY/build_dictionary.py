b30e7a627418d6b031dce2a0a94f691ae18a23f8
rn25-long-app <bhlong3112@gmail.com>
2024-08-07 01:35:32 +0700

    complete practice BUILD_DICTIONARY

--- Introduce type DICTIONARY/build_dictionary.py
+++ Introduce type DICTIONARY/build_dictionary.py
@@ -12,16 +12,16 @@
 dictionary = {}
 while(True):
     print('Please select the function (by number): ')
-    print('''
-       1. Add a new vocabulary word (with its meaning) to the dictionary.
-       2. Look up the meaning of a vocabulary word.
-       3. Update the meaning of vocabulary.
-       4. Allows users to delete a word from the dictionary .
-       5. Allows users to delete entire vocabulary.
-       6. Allows users to print out the entire vocabulary.
-       7. Allows users to print out the entire dictionary according to the structure: 'VOCABULARY', 'MEANING'.
-       8. End program.
-    ''')
+    # print('''
+    #    1. Add a new vocabulary word (with its meaning) to the dictionary.
+    #    2. Look up the meaning of a vocabulary word.
+    #    3. Update the meaning of vocabulary.
+    #    4. Allows users to delete a word from the dictionary .
+    #    5. Allows users to delete entire vocabulary.
+    #    6. Allows users to print out the entire vocabulary.
+    #    7. Allows users to print out the entire dictionary according to the structure: 'VOCABULARY', 'MEANING'.
+    #    8. End program.
+    # ''')
     select = int(input('Enter your selection: '))
     if(select == 1 or select == 3):
         Vocabulary = input('Please enter vocabulary: ')
@@ -41,16 +41,20 @@ while(True):
         dictionary.pop(Vocabulary)
         print('Deleted data')
     elif(select == 5):
-        Vocabulary.clear()
+        dictionary.clear()
         print('Deleted all data')
     elif(select == 6):
         print('List of vocabulary words in the dictionary: ')
         for x in dictionary.keys():
          print(x)
     elif(select == 7):
-        print('List of vocabulary words in the dictionary: ')
-        for x, y in dictionary.items():
-         print(x, ':', y)
+        if(dictionary):
+          print('List of vocabulary words in the dictionary: ')
+          for x, y in dictionary.items():
+            print(x, ':', y)
+        else:
+           print('Dictionary is not data')
+
     
     elif(select == 8):
         break
---
+++ Introduce type DICTIONARY/tempCodeRunnerFile.py
@@ -0,0 +1,10 @@
+print('''
+    #    1. Add a new vocabulary word (with its meaning) to the dictionary.
+    #    2. Look up the meaning of a vocabulary word.
+    #    3. Update the meaning of vocabulary.
+    #    4. Allows users to delete a word from the dictionary .
+    #    5. Allows users to delete entire vocabulary.
+    #    6. Allows users to print out the entire vocabulary.
+    #    7. Allows users to print out the entire dictionary according to the structure: 'VOCABULARY', 'MEANING'.
+    #    8. End program.
+    # ''')
